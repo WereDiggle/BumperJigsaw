8e684281e8098d501e554f28cbbad704e0d5d5c1
Allison Artemis Zou <allison.artemis.zou@gmail.com>
2022-05-21 13:19:28 -0400

    Pieces no longer retain velocity and force after release

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -47,6 +47,10 @@ func _input_event(viewport, event, shape_idx):
 		drag_double.transform = transform
 		drag_double.can_drag = true
 		drag_double.click_offset = get_global_mouse_position() - global_position
+		linear_velocity = Vector2(0,0)
+		angular_velocity = 0
+		applied_force = Vector2(0,0)
+		applied_torque = 0 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
