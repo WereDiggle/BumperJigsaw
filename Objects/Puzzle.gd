5566b8b4d8f6479914bbc0af09ca6f35958ad714
Allison Artemis Zou <allison.artemis.zou@gmail.com>
2022-05-28 18:16:01 -0400

    Puzzle spawn config distance and jiggle

--- Objects/Puzzle.gd
+++ Objects/Puzzle.gd
@@ -10,12 +10,18 @@ export(Texture) var image
 export var puzzle_width = 1 
 export var puzzle_height = 1 
 
+export var spawn_distance = 200
+export var spawn_jiggle = 50
+
+func rand_vector():
+	return Vector2(rand_range(-1, 1), rand_range(-1, 1))
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for h in puzzle_height:
 		for w in puzzle_width:
 			var piece_group = PieceGroup.instance().init(Vector2(w, h), Vector2(puzzle_width, puzzle_height), image)
-			piece_group.position = Vector2(w, h) * 150
+			piece_group.position = Vector2(w, h) * spawn_distance + rand_vector() * spawn_jiggle
 			add_child(piece_group)
 
 	pass # Replace with function body.
