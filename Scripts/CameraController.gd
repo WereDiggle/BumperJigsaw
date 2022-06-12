fae4893a8a55c46656c4cd5d2176dfbd76e514cd
Allison Artemis Zou <allison.artemis.zou@gmail.com>
2022-06-11 21:56:39 -0400

    Adjust camera zoom speed

--- Scripts/CameraController.gd
+++ Scripts/CameraController.gd
@@ -11,7 +11,7 @@ func _ready():
 	pass # Replace with function body.
 
 export var speed = 500
-export var zoom_speed = 1
+export var zoom_speed = 0.75
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
